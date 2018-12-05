61c38aead58d7f514a8e0f72f670e7b88bf8ca1a
Unknown <aderrien@hotmail.fr>
2018-12-05 10:58:07 +0100

    Bug config

--- src/main/java/sopra/formation/projet/repository/LoginRepository.java
+++ src/main/java/sopra/formation/projet/repository/LoginRepository.java
@@ -9,7 +9,7 @@ import sopra.formation.projet.model.Login;
 
 public interface LoginRepository extends JpaRepository<Login, String> {
 		
-	@Query("select u from Users u left join fetch u.roles where u.username=?1")
+	@Query("select u from Login u left join fetch u.roles where u.username=?1")
 	public Optional<Login> findByIdWithRoles(String username);
 		
 }
